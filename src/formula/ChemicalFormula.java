0817944b3c0e0c11b530e00e5c2f9d4188b452ce
ShaunHoward <smh150@case.edu>
2014-11-05 03:04:43 -0500

    Working on parentheses multiplier.

--- src/formula/ChemicalFormula.java
+++ src/formula/ChemicalFormula.java
@@ -1,6 +1,9 @@
 package formula;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.IllegalFormatException;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -24,6 +27,8 @@ public class ChemicalFormula {
                     "([A-Z][a-z]?[2-9]?)*)" +
                     "|([A-Z][a-z]?[2-9]?)+");
 
+    static Pattern NUMBER = Pattern.compile("[2-9]");
+
     /**
      * Takes a string from the standard in and determines if it is
      * a syntactically valid chemical formula.
@@ -46,7 +51,9 @@ public class ChemicalFormula {
             //Prints whether the chemical formula is syntactically valid.
             System.out.println(analyzeFormulaCompleteness(formula));
         } catch (IOException ioe) {
-            System.out.println("I/O Exception occurred while reading std in.");
+            System.err.println("I/O Exception occurred while reading std in.");
+        } catch (IllegalArgumentException iae) {
+            System.out.println("F");
         }
     }
 
@@ -58,7 +65,7 @@ public class ChemicalFormula {
      * @param formula - the formula to check for syntactical completeness
      * @return whether the formula is a syntactically valid chemical formula
      */
-    private static String analyzeFormulaCompleteness(String formula) {
+    private static String analyzeFormulaCompleteness(String formula) throws IllegalArgumentException {
         FormulaValidator validator = new FormulaValidator();
         //Validate the syntactical soundness of the formula.
         boolean potentialFormula = validator.validateFormula(formula);
@@ -81,7 +88,7 @@ public class ChemicalFormula {
      *
      * @param formula - the formula to check for syntactical soundness
      */
-    private static String analyzeFormulaSoundness(String formula){
+    private static String analyzeFormulaSoundness(String formula) throws IllegalArgumentException {
         String matches = "";
         Matcher matcher = FORMULA_PATTERN.matcher(formula);
         if(matcher.matches()){
@@ -90,6 +97,46 @@ public class ChemicalFormula {
             matches = "F";
         }
         return matches;
+    }
+
+    private static List stripParentheses(String formula) throws IllegalArgumentException {
+        List components = new ArrayList();
+        String subFormula = "";
+        int endParenIndex = 0;
+
+        if (formula.contains("(")){
+            while(formula.contains("(")){
+                endParenIndex = formula.indexOf(")");
+                subFormula = formula.substring(formula.lastIndexOf("("), endParenIndex);
+                checkParenthesesMultiplier(formula, endParenIndex);
+                components.add(subFormula);
+                formula.replace(subFormula, "");
+            }
+        }
+    }
+
+    /**
+     * Checks the multiplier that follows the parentheses of a chemical formula.
+     * Throws an exception if the multiplier is not between 2 and 9.
+     *
+     * @param formula - the formula to check the multiplier in
+     * @param endParenIndex - where to begin checking for the multiplier at
+     * @throws IllegalFormatException - thrown when the multiplier is not between 2 and 9
+     */
+    private static void checkParenthesesMultiplier(String formula, int endParenIndex) throws IllegalFormatException {
+        StringBuilder formulaBuilder = new StringBuilder(formula);
+        int currIndex = endParenIndex + 1;
+        StringBuilder numberBuilder = new StringBuilder();
+        while(Character.isDigit(formulaBuilder.charAt(currIndex))){
+            numberBuilder.append(formulaBuilder.charAt(currIndex));
+            formulaBuilder.deleteCharAt(currIndex);
+            currIndex++;
+        }
+        Matcher matcher = NUMBER.matcher(numberBuilder.toString());
+        if (!matcher.matches()){
+            throw new IllegalArgumentException("Incorrect multipliers following parentheses.");
+        }
+        formula = formulaBuilder.toString();
     }
 
 }
