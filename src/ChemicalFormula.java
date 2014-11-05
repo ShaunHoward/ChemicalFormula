fdf5159f97bad2e3ec965abfdb4e5c9eff5af8f5
ShaunHoward <smh150@case.edu>
2014-11-05 00:46:52 -0500

    Setup layout for syntax checking.

--- src/ChemicalFormula.java
+++ src/ChemicalFormula.java
@@ -21,6 +21,7 @@ public class ChemicalFormula {
      */
     public static void main(String[] args) {
         String formula = "";
+
         try (FileInputStream fis = new FileInputStream(new File(args[0]));
              BufferedReader br = new BufferedReader(new InputStreamReader(fis, args[1]))) {
             String appendage;
@@ -30,12 +31,44 @@ public class ChemicalFormula {
                 formula = formula + "\n" + appendage;
             }
 
-            //Validate the syntactical context of the formula.
-            //validateFormula(formula);
-
+            System.out.println(analyzeFormulaPotential(formula));
         } catch (IOException ioe) {
-            System.out.println("I/O Exception occurred.");
+            System.out.println("I/O Exception occurred while reading std in.");
         }
 
+    }
+
+    /**
+     * Analyzes the syntactical soundness in terms of chemical formulas
+     * for the given formula. Returns a string "T" if the formula
+     * can be a chemical formula, otherwise, returns "F".
+     *
+     * @param formula - the formula to check for syntactical soundness
+     * @return whether the formula is syntactically a chemical formula
+     */
+    private static String analyzeFormulaPotential(String formula) {
+        FormulaValidator validator = new FormulaValidator();
+        //Validate the syntactical context of the formula.
+        boolean potentialFormula = validator.validateFormula(formula);
+        String potential = "";
+
+        if (potentialFormula){
+            potential = analyzeFormulaSyntax(formula);
+        } else {
+            potential = "F";
+        }
+
+        return potential;
+    }
+
+    /**
+     * Analyzes the syntax of the input formula string.
+     * Returns a string "T" if the input formula is
+     * syntactically valid, otherwise returns a string "F".
+     *
+     * @param formula - the formula to check the syntax of
+     */
+    private static String analyzeFormulaSyntax(String formula){
+        return "";
     }
 }
